6ecd31ac936675479dccde7617804d616b6d0e45
JIN TAEYANG <sun@jupiterflow.com>
2021-03-09 19:12:02 +0900

    Create HomeController.java

--- workspace/spring/SpringExample/src/main/java/Controller/HomeController.java
+++ workspace/spring/SpringExample/src/main/java/Controller/HomeController.java
@@ -1,3 +1,14 @@
+/*
+    Copyright (c) 2021 Aaron(JIN, Taeyang).
+    All rights reserved. This program and the accompanying materials
+    are made available under the terms of the GNU Lesser General Public License, version 3
+    which accompanies this distribution, and is available at
+    https://www.gnu.org/licenses/lgpl-3.0.html
+    
+    Contributors:
+        Aaron(JIN, Taeyang) - 
+*/
+
 package Controller;
 
 import org.springframework.stereotype.Controller;
